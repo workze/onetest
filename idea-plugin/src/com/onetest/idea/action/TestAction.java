e48ca3a058cee70a15db1b30c74d55f165375d1d
guize <guize.wgz@alibaba-inc.com>
2021-08-17 17:31:59 +0800

    添加action

---
+++ idea-plugin/src/com/onetest/idea/action/TestAction.java
@@ -0,0 +1,12 @@
+package com.onetest.idea.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class TestAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        System.out.println("actioned!");
+    }
+}
