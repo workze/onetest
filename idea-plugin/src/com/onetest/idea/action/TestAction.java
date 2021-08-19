2b5d62b5ee31dbabd364d20a601fb1d81366f6d7
guize <guize.wgz@alibaba-inc.com>
2021-08-19 17:51:07 +0800

    添加浏览器工具

--- idea-plugin/src/com/onetest/idea/action/TestAction.java
+++ idea-plugin/src/com/onetest/idea/action/TestAction.java
@@ -1,5 +1,6 @@
 package com.onetest.idea.action;
 
+import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
@@ -11,5 +12,8 @@ public class TestAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent e) {
         System.out.println("actioned!");
+
+        // BrowserUtil.browse("https://www.baidu.com");
+
     }
 }
