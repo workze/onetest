016dda708d11df7b12aa953b7c6fa559140ecfdc
guize <guize.wgz@alibaba-inc.com>
2021-08-26 20:14:46 +0800

    idea 打开文件

--- idea-plugin/src/com/onetest/idea/action/TestAction.java
+++ idea-plugin/src/com/onetest/idea/action/TestAction.java
@@ -1,18 +1,28 @@
 package com.onetest.idea.action;
 
 import com.intellij.execution.filters.BrowserHyperlinkInfo;
+import com.intellij.execution.filters.FileHyperlinkInfo;
+import com.intellij.execution.filters.OpenFileHyperlinkInfo;
 import com.intellij.execution.ui.ConsoleView;
 import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.ide.browsers.OpenUrlHyperlinkInfo;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.psi.PsiType;
+import com.intellij.psi.util.PsiTypesUtil;
 import com.onetest.idea.toolwindow.ConsoleViewHolder;
+import com.onetest.idea.util.ConsoleLogger;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.TimeUnit;
 
@@ -38,15 +48,32 @@ public class TestAction extends AnAction {
 
         // 打开浏览器, 跳转到网页
         final ConsoleView consoleView = ConsoleViewHolder.getInstance(project).getConsoleView();
+        ConsoleLogger logger = new ConsoleLogger(consoleView);
 
         // 控制台输出超链接
-        consoleView.printHyperlink("http://www.baidu.com\n", new BrowserHyperlinkInfo("http://www.baidu.com"));
+        logger.log("BrowserHyperlinkInfo:");
+        consoleView.printHyperlink("前往百度\n", new BrowserHyperlinkInfo("http://www.baidu.com"));
 
+        // 控制台输出超链接
+        logger.log("OpenUrlHyperlinkInfo:");
+        consoleView.printHyperlink("前往百度\n", new OpenUrlHyperlinkInfo("http://www.baidu.com"));
+
+
+        // 打开文件
+        final VirtualFile abcFile = VirtualFileManager.getInstance().findFileByUrl("file:///tmp/abc");
+        logger.log("OpenUrlHyperlinkInfo:");
+        consoleView.printHyperlink("打开文件\n", new OpenFileHyperlinkInfo(new OpenFileDescriptor(e.getProject(), abcFile)));
+
+        // 打开文件2
+
+
+        // 输出当前线程名
         consoleView.print(Thread.currentThread().getName(), ConsoleViewContentType.NORMAL_OUTPUT);
 
         // 滚动条滚到最后
         consoleView.requestScrollingToEnd();
 
+        // 异步输出
         ApplicationManager.getApplication().executeOnPooledThread(() -> {
             ApplicationManager.getApplication().runReadAction(() -> {
                 consoleView.print(Thread.currentThread().getName() + "\n", ConsoleViewContentType.NORMAL_OUTPUT);
@@ -65,7 +92,7 @@ public class TestAction extends AnAction {
     @Override
     public void update(@NotNull AnActionEvent e) {
         final ConsoleView consoleView = ConsoleViewHolder.getInstance(e.getProject()).getConsoleView();
-        consoleView.print("AnAction.update()\n", ConsoleViewContentType.NORMAL_OUTPUT);
+        // consoleView.print("AnAction.update()\n", ConsoleViewContentType.NORMAL_OUTPUT);
 
         // Get required data keys
         final Project project = e.getProject();
