02411e5f2521824f788e60af3dc3d744172ed442
guize <guize.wgz@alibaba-inc.com>
2021-08-27 09:34:40 +0800

    test fastjson

--- lang/src/main/java/com/onetest/lang/FastJsonMain.java
+++ lang/src/main/java/com/onetest/lang/FastJsonMain.java
@@ -1,6 +1,7 @@
 package com.onetest.lang;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 /**
  * @author guize
@@ -10,6 +11,9 @@ public class FastJsonMain {
     public static void main(String[] args) {
         System.out.println(JSON.parseObject(JSON.toJSONString("abc"), String.class));
         System.out.println(JSON.toJSONString("abc"));
-        System.out.println(JSON.parseObject("abc", String.class));
+        // System.out.println(JSON.parseObject("abc", String.class));
+
+        final Double aDouble = JSON.parseObject("1.2", double.class);
+
     }
 }
