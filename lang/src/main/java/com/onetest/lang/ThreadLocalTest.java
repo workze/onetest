ae03ef21f9487069846cd60e63ab48fe729d7607
wangguize <wangguize@corp.netease.com>
2022-02-15 11:58:45 +0800

    add test

---
+++ lang/src/main/java/com/onetest/lang/ThreadLocalTest.java
@@ -0,0 +1,13 @@
+package com.onetest.lang;
+
+/**
+ * @author wangguize
+ * @date 2022/2/14
+ */
+public class ThreadLocalTest {
+
+    public static void main(String[] args) {
+        ThreadLocal<String> stringThreadLocal = new ThreadLocal<>();
+    }
+
+}
