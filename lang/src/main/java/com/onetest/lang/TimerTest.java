75f51a344382535fca4b796dd6bb5052a4eaef01
wangguize <wangguize@corp.netease.com>
2022-02-15 14:39:25 +0800

    add test

---
+++ lang/src/main/java/com/onetest/lang/TimerTest.java
@@ -0,0 +1,14 @@
+package com.onetest.lang;
+
+import java.util.Timer;
+
+/**
+ * @author wangguize
+ * @date 2022/2/15
+ */
+public class TimerTest {
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.cancel();
+    }
+}
